a90c42800ddf8fe5be1fcc79793f298de26a31e9
Bérengère <berengere.mathieu@makina-corpus.com>
2017-12-03 16:21:05 +0100

    Register item and tag to admin app

--- website/cv/admin.py
+++ website/cv/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models.tag import Tag
+from .models.item import Item
 
-# Register your models here.
+admin.site.register(Tag)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+
+admin.site.register(Item, ItemAdmin)
