a1d978e1ce37caceaa80e3f6263f20561e2a4c08
Bérengère <berengere.mathieu@makina-corpus.com>
2017-12-03 16:14:45 +0100

    Add item serializer

--- website/serializers.py
+++ website/serializers.py
@@ -1,5 +1,8 @@
+from rest_framework.serializers import Serializer
 from rest_framework.serializers import ModelSerializer
 from website.cv.models.tag import Tag
+from rest_framework.serializers import CharField
+from rest_framework.serializers import ImageField
 
 
 class TagSerializer(ModelSerializer):
@@ -10,3 +13,14 @@ class TagSerializer(ModelSerializer):
     class Meta:
         model = Tag
         fields = ('name',)
+
+
+class ItemSerializer(Serializer):
+    """
+    Item serializer
+    """
+    title = CharField(required=True)
+    description = CharField(required=False, allow_blank=True, \
+                            style={'base_template': 'textarea.html'})
+    image = ImageField(required=False)
+    tags = TagSerializer(read_only=True, many=True)
